d9ff27948376b96fdbb3a663252b64f26123b67a
datboiidom <dominicrmbl@gmail.com>
2026-04-07 18:29:05 +0000

    fgretgfdfgdf

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 console.log("Script started");
 
+//Variable to track current number
+let currentNum = 1;
+let level = 1;
+
 function start() {
-    console.log("click")
+    console.log("click");
     //console.log(Math.random()* window.innerWidth);
+    startLevel1();
+}
+
+function startLevel1() {
+    currentNum = 1;
+    level = 1;
     setTimeout(makeCircle, 100, 1);
     setTimeout(makeCircle, 500, 2);
     setTimeout(makeCircle, 1000, 3);
-    setTimeout(makeCircle, 1300, 4);
+}
+
+function startLevel2() {
+    currentNum = 1;
+    level = 2;
+    setTimeout(makeCircle, 100, 1);
+    setTimeout(makeCircle, 500, 2);
+    setTimeout(makeCircle, 1000, 3);
+    setTimeout(makeCircle, 1500, 4);
     setTimeout(makeCircle, 2000, 5);
-    setTimeout(makeCircle, 2300, 6);
 }
 
 
@@ -21,7 +38,7 @@ function makeCircle(num) {
     circle.style.width = "100px";
     circle.style.height = "100px"
     circle.style.color = "darkred";
-    circle.style.backgroundColor = "deepskyblue";
+    circle.style.backgroundColor = "pink";
     circle.style.borderRadius = "90px";
     circle.style.alignContent = "center";
     circle.style.textAlign = "center";
@@ -53,4 +70,15 @@ function getRandomY() {
 function checkForRemove(event) {
     let circle = event.target;
     console.log(circle.innerText);
+    if (circle.innerText == currentNum){
+        circle.remove();
+        currentNum++;
+        if (currentNum == 4 && level == 1) {
+            startLevel2();
+        }
+        else if (currentNum == 6 && level == 1) {
+            
+        }
+    }
 }
+
